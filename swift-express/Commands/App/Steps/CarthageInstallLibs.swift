b6c3ae955b1f1edf6fd34afc1052468734cb5338
Daniel Leping <dileping@gmail.com>
2016-02-05 01:40:52 +0200

    Copyrights

--- swift-express/Commands/App/Steps/CarthageInstallLibs.swift
+++ swift-express/Commands/App/Steps/CarthageInstallLibs.swift
@@ -1,10 +1,22 @@
+//===--- CarthageInstallLibs.swift -------------------------------===//
+//Copyright (c) 2015-2016 Daniel Leping (dileping)
 //
-//  CarthageInstallLibs.swift
-//  swift-express
+//This file is part of Swift Express Command Line
 //
-//  Created by Yegor Popovych on 2/1/16.
-//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//Swift Express Command Line is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
 //
+//Swift Express Command Line is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Swift Express Command Line. If not, see <http://www.gnu.org/licenses/>.
+//
+//===-------------------------------------------------------------===//
 
 import Foundation
 
