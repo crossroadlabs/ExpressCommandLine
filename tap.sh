8b57280dc5669d521077d2cec60a136d14624353
Daniel Leping <dileping@gmail.com>
2016-02-18 17:06:35 +0200

    sed test

--- tap.sh
+++ tap.sh
@@ -2,7 +2,9 @@
 
 git clone "$TAP_REPO_URL" "$TAP_DIR"
 cd "$TAP_DIR"
+cat Formula/swift-express.rb
 sed "s/version \"[\.0-9]*\"/version \"${VERSION}\"/g" Formula/swift-express.rb > Formula/swift-express.rb
+cat Formula/swift-express.rb
 git add Formula/swift-express.rb
 git commit -m "Updated swift-express version to ${VERSION}"
 git pull
