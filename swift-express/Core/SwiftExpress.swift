222bb29fc3db19b4a34e50458af96827024626fa
Yehor Popovych <popovych.yegor@gmail.com>
2016-03-06 00:06:28 +0200

    Moved version to SwiftExpress.swift

--- swift-express/Core/SwiftExpress.swift
+++ swift-express/Core/SwiftExpress.swift
@@ -21,16 +21,20 @@
 import Commandant
 import Foundation
 
+let CMD_LINE_VERSION = "0.2.0"
+
 #if os(Linux)
+    let IS_LINUX = true
+    
     let DEFAULTS_USE_SPM = true
     let DEFAULTS_USE_XCODE = false
     let DEFAULTS_BUILD_DISPATCH = false
-    let IS_LINUX = true
 #else
+    let IS_LINUX = false
+    
     let DEFAULTS_USE_SPM = false
     let DEFAULTS_USE_XCODE = true
     let DEFAULTS_BUILD_DISPATCH = true
-    let IS_LINUX = false
 #endif
 
 let DEFAULTS_USE_CARTHAGE = DEFAULTS_USE_XCODE
